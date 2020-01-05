93fce9eeb678dadab15bc28a9a86063af00e682a
Thái Thiên Vũ <thienvu1802@gmail.com>
2020-01-06 00:31:59 +0700

    Add demo

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,5 +1,6 @@
 import mtkhdt.n9.connection.ConnectionProvider;
 import mtkhdt.n9.connection.ConnectionType;
+import mtkhdt.n9.query.CompareOperator;
 
 import java.util.List;
 
@@ -17,26 +18,49 @@ public class Main {
 
         List<Student> students = (new Student())
                 .fetch();
-        System.out.println(students.toString());
+        System.out.println("Current student list: " + students.toString());
 
-//        Student student = new Student();
-//        student.mssv = "3";
-//        student.name = "Thien Vu";
-//        student.create();
-//        System.out.println(student.toString());
-//
-////        Student student = new Student();
-////        student.mssv = "2";
-////        student.name = "Hai Anh";;
-////        student.create();
-////        System.out.println(student.toString());
-//
-//        //vi du xoa theo dieu kien where
-//        List<Student> students = (new Student())
-//                .where("name", CompareOperator.EQUAL, "Hai Au")
-//                .orWhere("name", CompareOperator.EQUAL, "Hai Anh")
-//                .fetch();
+        //Demo insert
+        Student student = new Student();
+        student.mssv = "4";
+        student.name = "Thien Vu";
+        student.create();
+        System.out.println("Recently inserted: " + student.toString());
+        students = (new Student())
+                .fetch();
+        System.out.println("Current student list: " + students.toString());
 
+        //Demo self update
+        student.name = "Bich Van";
+        student.save();
+        System.out.println("Recently updated: " + student.toString());
+        students = (new Student())
+                .fetch();
+        System.out.println("Current student list: " + students.toString());
+
+        //Demo patch update
+        Student demoUpdateModel = new Student();
+        demoUpdateModel.name = "Demo update guy";
+        long rowsAffected = demoUpdateModel
+                .where("mssv", CompareOperator.EQUAL, "4")
+                .orWhere("mssv", CompareOperator.EQUAL, "1").update();
+        System.out.println("Rows affected: " + rowsAffected);
+        students = (new Student())
+                .fetch();
+        System.out.println("Current student list: " + students.toString());
+
+        //Demo self destroy
+        (new Student()).where("mssv", CompareOperator.EQUAL, "4").remove();
+        students = (new Student())
+                .fetch();
+        System.out.println("Current student list: " + students.toString());
+
+        Student demoDeleteModel = new Student();
+        rowsAffected = demoUpdateModel.where("mssv", CompareOperator.NOT_EQUAL, "").delete();
+        System.out.println("Rows affected: " + rowsAffected);
+        students = (new Student())
+                .fetch();
+        System.out.println("Current student list: " + students.toString());
     }
 }
 
